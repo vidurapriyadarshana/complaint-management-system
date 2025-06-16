fa23472a2f8fed3548ce3921f0373f614f8adb36
Vidura Priyadarshana <vidura200427@gmail.com>
2025-06-16 11:55:01 +0530

    Login Page Load Complete

--- src/main/java/edu/vidura/complaintmanagementsystem/controller/LoginServlet.java
+++ src/main/java/edu/vidura/complaintmanagementsystem/controller/LoginServlet.java
@@ -1,5 +1,22 @@
 package edu.vidura.complaintmanagementsystem.controller;
 
-public class LoginServlet {
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/api/v1/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/views/login.jsp");
+        rd.forward(req, resp);
+    }
+
 
 }
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -11,6 +11,6 @@
     <title>Title</title>
 </head>
 <body>
-
+    <h1>LogIN</h1>
 </body>
 </html>
