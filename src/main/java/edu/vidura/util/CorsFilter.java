3bea09504cf38236b9c0ac68eaf35e4611b5e960
Vidura Priyadarshana <vidura200427@gmail.com>
2025-06-20 01:13:11 +0530

    CorsFilter Add

---
+++ src/main/java/edu/vidura/util/CorsFilter.java
@@ -0,0 +1,21 @@
+package edu.vidura.util;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST");
+        httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        chain.doFilter(request, response);
+    }
+
+}
